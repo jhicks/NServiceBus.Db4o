8332af7ca253aa970912b4b83dd8644cb6bdbe9d
James Hicks <jhicks0300@gmail.com>
2010-03-10 19:08:13 -0600

    removed config stuff from saga persister and subscription storage projects

--- src/NServiceBus.SagaPersisters.Db4o/Db4oSagaPersisterConfig.cs
+++
@@ -1,6 +0,0 @@
-﻿using NServiceBus.Db4o;
-
-namespace NServiceBus.SagaPersisters.Db4o
-{
-    public class Db4oSagaPersisterConfig : Db4oConnectionConfig {}
-}
--- src/NServiceBus.Unicast.Subscriptions.Db4o/Db4oSubscriptionStorageConfig.cs
+++
@@ -1,8 +0,0 @@
-﻿using NServiceBus.Db4o;
-
-namespace NServiceBus.Unicast.Subscriptions.Db4o
-{
-    public class Db4oSubscriptionStorageConfig : Db4oConnectionConfig
-    {
-    }
-}
